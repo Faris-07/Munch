deeed9f1600f616f496332b2b38972bf6783e7bc
Faris-07 <farisuddin03@gmail.com>
2022-06-05 13:22:42 +0000

    Add list display to recipes admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -7,5 +7,6 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Recipe)
 class AdminRecipe(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'published_on')
     prepopulated_fields = {'slug': ('author', 'title')}
     summernote_fields = ('ingredients', 'method')
