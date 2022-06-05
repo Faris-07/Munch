e7e5a7eec1c66520389679456476de172e5b8a62
Faris-07 <farisuddin03@gmail.com>
2022-06-05 13:33:46 +0000

    Add Comment model to admin panel with search and filter functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -12,3 +12,10 @@ class AdminRecipe(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('author', 'title')}
     list_filter = ('published_on', 'author')
     summernote_fields = ('ingredients', 'method')
+
+@admin.register(Comment)
+class AdminComment(admin.ModelAdmin):
+
+    search_fields = ('name', 'email', 'body')
+    list_display = ('name', 'body', 'recipe', 'created_on')
+    list_filter = ('created_on', )
