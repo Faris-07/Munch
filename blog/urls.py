7fcf5995f4d31a11f13811ca0986ab63b2e6dbcb
Faris-07 <farisuddin03@gmail.com>
2022-06-07 15:15:31 +0000

    Add RecipeDetail url path to urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('recipes/', views.RecipeView.as_view(), name='recipes')
+    path('recipes/', views.RecipeView.as_view(), name='recipes'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name="recipe_detail")
 ] 
