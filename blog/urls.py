bdf4933069157ac9d011b34462f354a469eef223
Faris-07 <farisuddin03@gmail.com>
2022-06-08 15:56:57 +0000

    Add likes and comment counter to recipe_detail.html page

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('recipes/', views.RecipeView.as_view(), name='recipes'),
-    path('<slug:slug>/', views.RecipeDetail.as_view(), name="recipe_detail")
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name="recipe_detail"),
+    path('like/<slug:slug>', views.RecipeLike.as_view(), name="recipe_like"),
 ] 
