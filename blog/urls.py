2c6830e0a2a6246ff98f65876b19a1925041de91
Faris-07 <farisuddin03@gmail.com>
2022-06-14 10:23:30 +0000

    Add LikedRecipes url path to urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('add_recipe/', views.AddRecipe.as_view(), name='add_recipe'),
     path('edit_recipe/<int:pk>', views.EditRecipe.as_view(), name='edit_recipe'),
     path('delete_recipe/<str:pk>', views.delete_recipe, name='delete_recipe'),
+    path('liked_recipes/', views.LikedRecipes.as_view(), name='liked_recipes'),
+    path('search/', views.SearchRecipe, name='search_recipe'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name="recipe_detail"),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name="recipe_like"),
-    path('search', views.SearchRecipe, name='search_recipe'),
 ]
