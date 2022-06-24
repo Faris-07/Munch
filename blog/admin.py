ce579cdac645637113a756b79e577f4fbfe13967
Faris-07 <farisuddin03@gmail.com>
2022-06-24 19:07:46 +0000

    Add Docstrings to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class AdminRecipe(SummernoteModelAdmin):
-
+    """Admins recipe model features"""
     list_display = ('title', 'slug', 'published_on')
     search_fields = ['title', 'description', 'ingredients']
     prepopulated_fields = {'slug': ('author', 'title')}
@@ -15,7 +15,7 @@ class AdminRecipe(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class AdminComment(admin.ModelAdmin):
-
+    """Admins comment model features"""
     search_fields = ('name', 'email', 'body')
     list_display = ('name', 'body', 'recipe', 'created_on')
     list_filter = ('created_on', )
