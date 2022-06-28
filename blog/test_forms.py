0dba41397606c9d712b53dc087d4d404755c97bc
Faris-07 <farisuddin03@gmail.com>
2022-06-28 20:26:14 +0000

    Add test for recipe form description required

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -3,7 +3,7 @@ class TestRecipeForm(TestCase):
     Testing for Recipe Form
     '''
 
-    # Tests to check required fields are not blank
+    # Tests to check required fields
 
     def test_recipe_title_required(self):
         '''
@@ -13,3 +13,14 @@ class TestRecipeForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('title', form.errors.keys())
         self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_recipe_description_required(self):
+        '''
+        Test to ensure description is present
+        '''
+        form = RecipeForm({'description': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('description', form.errors.keys())
+        self.assertEqual(
+            form.errors['description'][0], 'This field is required.'
+            )
